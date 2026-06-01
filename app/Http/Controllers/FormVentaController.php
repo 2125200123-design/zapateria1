4f070ba23ba7abd3741185ed240b86671dff412d
Lexxus <2125200123@soy.utj.edu.mx>
2026-06-01 11:13:11 -0600

    m

--- app/Http/Controllers/FormVentaController.php
+++ app/Http/Controllers/FormVentaController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
+use App\Models\Empleado;
 use Illuminate\Http\Request;
 
 class FormVentaController extends Controller
 {
-        public function listado(){
-       // return "Este es el metodo para el select * from table";
-        return view('ventas/ventas');
+    public function listado()
+    {
+        $clientes = Cliente::all();
+        $empleados = Empleado::all();
+
+        return view('Ventas.ventas', compact('clientes', 'empleados'));
     }
 }
--- resources/views/ventas/ventas.blade.php
+++ resources/views/ventas/ventas.blade.php
@@ -102,7 +102,7 @@
     {{ isset($venta) ? 'Actualizar Venta' : 'Guardar Venta' }}
 </button>
 
-<a href="/Ventas_Tabla"
+<a href="/"
    class="mt-8 inline-block bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-3 rounded-xl">
     Regresar
 </a>
